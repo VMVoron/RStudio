c1d4125e802b6fc3d89379c1decd89ed738f976e
VMVoron <103144367+VMVoron@users.noreply.github.com>
2022-12-10 17:22:04 +0300

    Add files via upload

---
+++ LM.R
@@ -0,0 +1,32 @@
+# Data sets for linear regression https://people.sc.fsu.edu/~jburkardt/datasets/regression/regression.html
+data <- read.table('x03.txt',              # TXT data file indicated as string or full path to the file
+           header = TRUE)         # Character used to separate decimals of the numbers in the file
+#checking if reading is in correct format
+head(data)
+# Calculating the parameters of linear regression using lm
+lm.data <- lm(formula = Blood_Pressure ~ Age, data = data)
+lm.data$coefficients
+b0 <- lm.data$coefficient[1]
+b1 <- lm.data$coefficient[2]
+x1 <- as.numeric(min(data$Age))
+x2 <- as.numeric(max(data$Age))
+x <- seq(from = x1, to = x2, length.out = 70)
+y <- b0 + b1*x
+plot(data$Age, data$Blood_Pressure, main="Pair linear regression", xlab="Age (years)", ylab="Blood pressure (systolic)")
+grid()
+lines(x, y, col="red")
+summary(lm.data)
+# Linear regression model -97.0771 + 0.9493 * Age
+# То есть увеличение возраста на 10 лет увеличивает давление примерно на 9.5 единиц
+# Наибольшее положительное наблюдаемое отклонение от модельного: 21.153 пункта давления,
+# Наибольшее отрицательное: -19.354
+# Все коэффициенты значимы на уровне p-value < 0.001, это отмечено тремя звездочками (***) в выводе в таблице "Coefficients"
+# А также можно заметить низкие значение p-value самостоятельно в той же таблице Pr(>|t|) : 2.67e-16, 8.88e-09
+#Коэффициент детерминации, или R² (иногда читаемый как R-два), является еще одним показателем, который мы можем использовать для оценки модели, 
+# R² всегда будет между -∞ и 1
+# Среднеквадратичное отклонение (Adj. R-squared: 0.7015). Его значение довольно близко к единице, можно сказать, что модель довольно значима
+# Имеем степени свободы 1 и 27 
+# Посмотрим на F-критерий Фишера
+# F табл (a, k1, k2): a - значимость, k1 - количество факторов, k2 = n-k1-1
+# k1 = 1, k2 = 29-2 = 27
+# F табличное при увроне значимости 0.01 = 7.6, 66.81 > 7.6, значимость подтверждена по критерию Фишера
